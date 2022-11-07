bbd393fe531e25266e1c64c133cb3f795ce415c2
Evannfries <evann.fries@yahoo.com>
2022-11-07 11:54:41 -0600

    user schema created

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    profilePic:{
+        type: String,
+        default:''
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
