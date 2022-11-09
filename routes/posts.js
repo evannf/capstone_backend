997b652dce03c0e179d5b98e27a4abc4326093aa
Evannfries <evann.fries@yahoo.com>
2022-11-09 14:56:47 -0600

    get post route tested in postman

--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,15 @@ router.post('/', async (req, res) => {
 });
 
 //GET SINGLE POST
-
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 //GET POSTS FROM ALL USERS
 
 //UPDATE POST
