4b9996bc1bc78398e221a3a98919abe7497540e4
Evannfries <evann.fries@yahoo.com>
2022-11-09 15:08:50 -0600

    update post route working

--- routes/posts.js
+++ routes/posts.js
@@ -26,10 +26,25 @@ router.get("/:id", async (req, res) => {
       res.status(500).json(err);
     }
   });
-  
+
 //GET POSTS FROM ALL USERS
 
-//UPDATE POST
+//UPDATE POST ROUTE
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("post updated");
+      } else {
+        res.status(400).json("this was posted by another user");
+      }
+
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 //DELETE POST
 
