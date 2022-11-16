9060967a96749eb114ba2d2329c42953b9a5f874
Evannfries <evann.fries@yahoo.com>
2022-11-16 15:06:58 -0600

    minor changes

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const userRoutes = require('./routes/users.js');
 const registerRoutes = require('./routes/register.js');
 const loginRoutes = require('./routes/login.js');
 const postRoutes = require('./routes/posts.js')
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT||3001;
 
 dotenv.config();
 require('./config/db.connection');
 
+const cors = require('cors');
+
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+
+app.use(cors())
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+
 
 app.use('/users', userRoutes);
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
 app.use('/posts', postRoutes);
 
+const Posts = require('./models/post.js');
+const Users = require('./models/user.js');
 
 
 app.listen(PORT, () =>{
--- routes/register.js
+++ routes/register.js
@@ -9,13 +9,13 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try{
-        const hashPass = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+        const secretPass = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 
         //mongoose method save() used to create new or update documents
         //https://mongoosejs.com/docs/documents.html#updating-using-save  
         const newUser = new User({
             username: req.body.username,
-            password: hashPass,
+            password: secretPass,
         });
         
         const user = await newUser.save();
